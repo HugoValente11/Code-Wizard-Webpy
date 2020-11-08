c93dcfc0fe75330a8d63061f32ace98578bae981
Hugo Valente <hugomika68@gmail.com>
2020-11-08 11:06:17 +0100

    Add PostRegistration class to get User form

--- Controller.py
+++ Controller.py
@@ -7,7 +7,9 @@ Created on Wed Nov  4 15:12:36 2020
 import web
 
 urls = (
-        '/', 'Home'
+        '/', 'Home',
+        '/register', 'Register',
+        '/postregistration', 'PostRegistration'
         )
 
 render = web.template.render("Views/Templates", base="MainLayout")
@@ -18,6 +20,16 @@ class Home:
     def GET(self):
         return render.Home()
 
+
+class Register:
+    def GET(self):
+        return render.Register()
+
+
+class PostRegistration:
+    def POST(self):
+        data = web.input()
+        return data.username
 
 
 if __name__ == "__main__":
