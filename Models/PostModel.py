441fc986d8dfe50db81eefd8ae08191c5707621c
Hugo Valente <hugomika68@gmail.com>
2020-11-13 18:08:19 +0100

    Add relative time

--- Models/PostModel.py
+++ Models/PostModel.py
@@ -1,4 +1,6 @@
 from pymongo import MongoClient
+import datetime
+import humanize
 
 
 class PostModel:
@@ -8,7 +10,7 @@ class PostModel:
         self.Posts = self.db.posts
 
     def insert_posts(self, data):
-        self.Posts.insert({'username': data['username'], 'content': data['content']})
+        self.Posts.insert({'username': data['username'], 'content': data['content'], "date_added": datetime.datetime.now()})
 
         return True
 
@@ -16,6 +18,9 @@ class PostModel:
         posts = self.Posts.find()
         new_posts = []
         for post in posts:
+            post['timestamp'] = humanize.naturaltime(datetime.datetime.now() - post['date_added'])
+            print("Date_added", post['date_added'])
+            print("Timestamp: ", humanize.naturaltime(datetime.datetime.now() - post['date_added']))
             new_posts.append(post)
         return new_posts
 
@@ -23,6 +28,7 @@ class PostModel:
         all_posts = self.Posts.find({'username': user})
         new_posts = []
         for post in all_posts:
+            post['timestamp'] = humanize.naturaldate(datetime.datetime.now() - post['date_added'])
             new_posts.append(post)
         return new_posts
 
