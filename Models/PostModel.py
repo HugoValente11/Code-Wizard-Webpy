880ac12dc4ebb2c3234e83741fdba7b532ec1e16
Hugo Valente <hugomika68@gmail.com>
2020-11-13 18:14:23 +0100

    Add sort by date added

--- Models/PostModel.py
+++ Models/PostModel.py
@@ -15,7 +15,7 @@ class PostModel:
         return True
 
     def get_all_posts(self):
-        posts = self.Posts.find()
+        posts = self.Posts.find().sort('date_added', -1)
         new_posts = []
         for post in posts:
             post['timestamp'] = humanize.naturaltime(datetime.datetime.now() - post['date_added'])
@@ -25,7 +25,7 @@ class PostModel:
         return new_posts
 
     def get_user_posts(self, user):
-        all_posts = self.Posts.find({'username': user})
+        all_posts = self.Posts.find({'username': user}).sort('date_added, -1')
         new_posts = []
         for post in all_posts:
             post['timestamp'] = humanize.naturaldate(datetime.datetime.now() - post['date_added'])
